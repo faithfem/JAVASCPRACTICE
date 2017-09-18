872fc9c2ac2cc58f1b95bafdb0964f8fa0ed29cc
Faith <fmugure@intracitygeeks.org>
2017-09-18 03:25:35 +0000

    months

--- app.js
+++ app.js
@@ -11,3 +11,24 @@ num > 0;
 num--){
     console.log(num);
 }
+
+//PRACTICE #2 WHILE-LOOP
+var numSheep = 4;
+var monthNumber = 1;
+var monthsToPrint = 12;
+
+while (monthNumber <= monthsToPrint) {
+  numSheep = numSheep * 4;
+  console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
+  monthNumber++;
+}
+
+//PRACTICE #2 FOR-LOOP
+var numSheep = 4;
+var monthsToPrint = 12;
+
+for (var monthNumber = 1; monthNumber <= monthsToPrint; monthNumber++) {
+  numSheep = numSheep * 4;
+  console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
+}
+
