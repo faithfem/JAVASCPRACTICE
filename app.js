1b2b1f463995c2c05ec76d3f916e81ca2b421016
Faith <fmugure@intracitygeeks.org>
2017-09-18 04:18:13 +0000

    generators

--- app.js
+++ app.js
@@ -32,3 +32,19 @@ for (var monthNumber = 1; monthNumber <= monthsToPrint; monthNumber++) {
   console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
 }
 
+//WHILE-LOOP 4 GENERATORS AND FOR-LOOP 15 GENERATORS
+
+var currentGen = 1;
+var totalGen = 19;
+var totalMW = 0;
+
+while (currentGen <= 4) {
+  totalMW += 62;
+  console.log("Generator #" + currentGen + " is on, adding 62 MW, for a total of " + totalMW + " MW!");
+  currentGen++;
+}
+
+for (currentGen = 5; currentGen <= totalGen; currentGen++) {
+  totalMW += 124;
+  console.log("Generator #" + currentGen + " is on, adding 124 MW, for a total of " + totalMW + " MW!");
+}
